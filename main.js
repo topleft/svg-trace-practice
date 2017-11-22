e42bd1ebe36f5287c18c39406bad742616f56b40
Pete Jeffryes <pjeffryes@tendrilinc.com>
2017-11-21 20:10:18 -0700

    made path class with trace method

--- main.js
+++ main.js
@@ -1,94 +1,104 @@
 
-const s = Snap("#svg");
 const loops = [
     {
-      path: "M 400 500 \
+      data: "M 400 500 \
         a15,7,0,0,0,0,-120 \
         a15,7,0,0,0,0,120",
-      color: "#3FA",
+      color: "#B21212",
       timeout: 500,
-      duration: 1000,
+      duration: 1200,
     }
     ,
     {
-      path: "M 450 497 \
+      data: "M 450 497 \
         a3,17,0,0,0,0,-320 \
         a3,17,0,0,0,0,320",
-      color: "#3FA",
-      timeout: 250,
-      duration: 1000,
+      color: "#FFFC19",
+      timeout: 750,
+      duration: 1200,
     }
     ,
     {
-      path: "M 370 444 \
+      data: "M 370 444 \
         a3,7,0,0,0,0,-200 \
         a3,7,0,0,0,0,200",
-      color: "#3FA",
+      color: "#FF0000",
       timeout: 300,
-      duration: 1000,
+      duration: 1200,
     }
     ,
     {
-      path: "M 350 365 \
+      data: "M 350 365 \
         a3,7,-30,0,1,0,-150 \
         a3,7,-30,0,1,0,140",
-      color: "#3FA",
+      color: "#1485CC",
       timeout: 0,
-      duration: 1000,
+      duration: 1200,
     }
     ,
     {
-      path: "M 460 340 \
+      data: "M 460 340 \
         a2.3,7,-40,0,1,0,-150 \
         a2.3,7,-40,0,1,0,150",
-      color: "#3FA",
-      timeout: 400,
-      duration: 1000,
+      color: "#0971B2",
+      timeout: 800,
+      duration: 1200,
     }
     ,
     {
-      path: "M 418 380 \
+      data: "M 418 380 \
         a42,30,0,0,0,0,-106 \
         a38,30,0,0,0,0,110",
-      color: "#3FA",
-      timeout: 20,
-      duration: 1000,
+      color: "#0971B2",
+      timeout: 900,
+      duration: 1200,
     }
     ,
     {
-      path: "M 418 490 \
+      data: "M 418 490 \
         a30,30,0,0,0,0,-110 \
         a30,30,0,0,0,0,100",
-      color: "#3FA",
-      timeout: 1000,
-      duration: 1000,
+      color: "#1485CC",
+      timeout: 1200,
+      duration: 1200,
     }
 ];
 
-loops.forEach(animateLoop)
+class Path {
+  constructor(obj) {
+    this.data = obj.data; 
+    this.color = obj.color || '#000'; 
+    this.timeout = obj.timeout || 0; 
+    this.duration = obj.duration || 500;
+  }
 
-function animateLoop(loop) {
+  trace (paper) {
     setTimeout(() => {
-        const loopLength = Snap.path.getTotalLength(loop.path);
-        const circle = s.path({
-            path: Snap.path.getSubpath(loop.path, 0, 0),
-            stroke: loop.color,
-            fillOpacity: 0,
-            strokeWidth: 0,
+      const loopLength = Snap.path.getTotalLength(this.data);
+      const instantiatedPath = paper.path({
+        path: Snap.path.getSubpath(this.data, 0, 0),
+        stroke: this.color,
+        fillOpacity: 0,
+        strokeWidth: 0,
+      });
+      
+      Snap.animate(0, loopLength,
+        (function(step){ //step function
+          instantiatedPath.attr({
+            path: Snap.path.getSubpath(this.data, 0, step),
+            strokeWidth: 2
           });
-        
-        Snap.animate(0, loopLength,
-          function(step){ //step function
-            circle.attr({
-              path: Snap.path.getSubpath(loop.path, 0, step),
-              strokeWidth: 2
-            });
-            
-          }, // end of step function
-          loop.duration //duration
-        ); //Snap.animate
-      }, loop.timeout)
+          
+        }).bind(this), // end of step function (must bind to class)
+        this.duration //duration
+      ); //Snap.animate
+    }, this.timeout)
+  }
+  
 }
+const paper = Snap("#svg");
+const paths = loops.map((p) => new Path(p));
+paths.forEach((p) => p.trace(paper));
 
 
 
